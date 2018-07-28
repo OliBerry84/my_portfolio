8ffd183369caf656ba4b0ab22272fbd26ccdd593
Oli <oliver.berry84@gmail.com>
2018-07-28 19:42:17 +0100

    tidied landing page

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,31 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Portfolio extends Component{
+class Portfolio extends Component {
 
   topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
-  
+
   render() {
 
     return (
       <div>
 
-        <div className="topnav">
-          <a className = 'name' href='#projects'>Projects</a>
-          <a className = 'name' href='#about-me'>About Me</a>
-          <a className = 'name' href='#footer'>Contact</a>
+        <div className="topnav" id='mytopnav'>
+          <a className='name' href='#projects'>Projects</a>
+          <a className='name' href='#about-me'>About Me</a>
+          <a className='name' href='#footer'>Contact</a>
         </div>
 
-        <div className = 'intro-container'>
-          <p className = 'intro-text'>I am an aspiring front end developer/web designer, pursuing a career in Edinburgh, Scotland after completing a 16 week full stack course with the excellent people at CodeClan</p>
-          <img className = 'cerberusimage' src={require('./images/cerberus.png')} alt='cerberusdesigns'/>
+        <div className='intro-container'>
+          <p className='intro-text'>I am an aspiring software developer, pursuing a career in Edinburgh, Scotland after completing a 16 week full stack course with the excellent people at CodeClan</p>
+          <img className='cerberusimage' src={require('./images/cerberus.png')} alt='cerberusdesigns' />
         </div>
 
-</div>
-); // return
-} // render
+      </div>
+    ); // return
+  } // render
 } // class
 
 
